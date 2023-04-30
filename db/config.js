7bcdbd20c0f63737360103e6e3c906500124c666
owais422 <awaisanees422@gmail.com>
2023-04-30 10:35:56 +0500

    server

--- db/config.js
+++ db/config.js
@@ -1,3 +1,13 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/restaurant-app');
+// mongoose.connect('mongodb://127.0.0.1:27017/restaurant-app');
+mongoose
+  .connect(
+    "mongodb+srv://awaisanees422:owais422@cluster0.xfrazbk.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("CONNECTED");
+  })
+  .catch(() => {
+    console.log("ERROR");
+  });
